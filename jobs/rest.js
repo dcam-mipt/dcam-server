8d30cbbd39045c188f6e28934cd302caeaab1b7c
mityabeldii <mityabeldii@gmail.com>
2019-03-18 22:25:17 +0300

    18.03.2019🌿22:25

--- jobs/rest.js
+++ jobs/rest.js
@@ -42,9 +42,8 @@ let updateActivity = (user) => new Promise((resolve, reject) => {
 })
 
 let become = (request) => new Promise((resolve, reject) => {
-    // let sessionToken = request.headers.sessiontoken
-    let sessionToken = `r:` + request.headers.accept.split(`r:`)[1]
-    console.log(`sessionToken:`, sessionToken)
+    let sessionToken = request.headers.sessiontoken
+    // let sessionToken = `r:` + request.headers.accept.split(`r:`)[1]
     if (!sessionToken) {
         reject({
             error: `invalid sessoin token`,
