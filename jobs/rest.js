73ae44e8104e81fccdf2618f623c5a75912f4145
mityabeldii <mityabeldii@gmail.com>
2019-03-20 10:52:05 +0300

    20.03.2019🌿10:52

--- jobs/rest.js
+++ jobs/rest.js
@@ -324,7 +324,7 @@ server.get(`/machines/get`, (request, response, next) => {
         .then((user) => {
             new Parse.Query(`Machines`)
                 .find()
-                .then((d) => { response.send(d) })
+                .then((d) => { response.send(d.createdAt) })
                 .catch((d) => { response.send(d); console.error(d) })
         })
         .catch((d) => { response.send(d); console.error(d) })
