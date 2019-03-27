c36864e228e599030a5705029d215a6d54e89f06
mityabeldii <mityabeldii@gmail.com>
2019-03-27 14:46:14 +0300

    27.03.2019🌿14:46

--- jobs/rest.js
+++ jobs/rest.js
@@ -352,6 +352,7 @@ server.get(`/user/auth/:login/:password`, (request, response, next) => {
         .then((user) => {
             new Parse.Query(`Session`)
                 .equalTo(`user`, user)
+                .first()
                 .then((d) => { response.send(d) })
                 .catch((d) => { response.send(d) })
         })
