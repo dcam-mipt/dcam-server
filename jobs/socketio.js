fdf7976ab9f6b150e6d399ac7950f0ad56709abe
mityabeldii <mityabeldii@gmail.com>
2019-04-16 01:27:59 +0300

    16.04.2019🌿01:27

--- jobs/socketio.js
+++ jobs/socketio.js
@@ -1,10 +1,25 @@
 /*eslint-disable no-unused-vars*/
 const server = require('http').createServer();
-const io = require('socket.io')(server);
-io.on('connection', client => {
-    client.on('event', data => { console.log(`event`) });
-    client.on('disconnect', () => { console.log(`disconnect`) });
+const io = require('socket.io')({
+    path: '/websocket',
+    serveClient: false,
 });
+
+// either
+const server = require('http').createServer();
+
+io.attach(server, {
+    pingInterval: 10000,
+    pingTimeout: 5000,
+    cookie: false
+});
+
 server.listen(3000);
 
+// or
+io.attach(3000, {
+    pingInterval: 10000,
+    pingTimeout: 5000,
+    cookie: false
+});
 /*eslint-enable no-unused-vars*/
