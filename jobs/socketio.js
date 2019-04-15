7f7ff3c083bca250345ce9a9e13472dff4350900
mityabeldii <mityabeldii@gmail.com>
2019-04-16 00:03:14 +0300

    16.04.2019🌿00:03

--- jobs/socketio.js
+++ jobs/socketio.js
@@ -1,14 +1,22 @@
 /*eslint-disable no-unused-vars*/
-const server = require('http').createServer();
+var express = require('express');
+var app = express();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
 
-const io = require('socket.io')(server, {
-    path: '/test',
-    serveClient: false,
-    // below are engine.IO options
-    pingInterval: 10000,
-    pingTimeout: 5000,
-    cookie: false
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "http://localhost:8100"); //The ionic server
+    res.header("Access-Control-Allow-Credentials", "true");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
 });
+var port = Number(process.env.PORT || 3000);
 
-server.listen(3000);
+io.on('connection', function (socket) {
+    console.log('ping-pong started');
+    socket.emit('news', { hello: 'world' });
+    socket.on('my other event', function (data) {
+        console.log(data);
+    });
+});
 /*eslint-enable no-unused-vars*/
