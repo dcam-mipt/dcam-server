f62b2d01290e3208a6523f6a29d30da7742c03ed
mityabeldii <mityabeldii@gmail.com>
2019-04-15 22:53:26 +0300

    15.04.2019🌿22:53

--- jobs/socketio.js
+++ jobs/socketio.js
@@ -1,5 +1,5 @@
 /*eslint-disable no-unused-vars*/
 const io = require('socket.io')();
 io.on('connection', client => { console.log(`socket.io: new connection -`, client); });
-io.listen(3000);
+io.listen(6369);
 /*eslint-enable no-unused-vars*/
