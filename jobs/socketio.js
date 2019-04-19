c13fad8ad6e18af14c5211f8ef927a7af94aafdd
mityabeldii <mityabeldii@gmail.com>
2019-04-20 00:23:31 +0300

    20.04.2019🌿00:23

--- jobs/socketio.js
+++ jobs/socketio.js
@@ -1,26 +1,6 @@
 /*eslint-disable no-unused-vars*/
-var express = require('express');
-var app = express();
-var http = require('http')
-var cors = require('cors')
-app.use(cors())
-app.use(function (req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "X-Requested-With");
-    res.header("Access-Control-Allow-Headers", "Content-Type");
-    res.header("Access-Control-Allow-Methods", "PUT, GET, POST, DELETE, OPTIONS");
-    res.end('test');
-    next();
-});
-app.get('/test', function (req, res, next) {
-    res.json({ msg: 'This is CORS-enabled for all origins!' })
-})
-
-app.listen(3000, function () {
-    console.log('CORS-enabled web server listening on port 3000')
-})
-var server = http.createServer(app);
-const io = require('socket.io')(server, { origins: '*:*' });
+var server = http.createServer();
+const io = require('socket.io')(server);
 
 io.on('connection', (socket) => {
     console.log(`new connection`);
