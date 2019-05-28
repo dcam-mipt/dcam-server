7e28ec76482307b9e945a133e63975f2dbf3dfb2
mityabeldii <mityabeldii@gmail.com>
2019-05-29 00:38:30 +0300

    29.05.2019🌿00:38

--- scripts/socketio.js
+++ scripts/socketio.js
@@ -1,6 +1,6 @@
 /*eslint-disable no-unused-vars*/
 const server = require('http').createServer((req, res) => { res.end('test') });
-const io = require('socket.io')(server, { path: '/test' });
+const io = require('socket.io')(server);
 
 io.on('connection', (socket) => {
     console.log(`new connection`);
