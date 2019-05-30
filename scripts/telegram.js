3d60626eab8cc5b8fe1b4a57ef521f01f29f887b
mityabeldii <mityabeldii@gmail.com>
2019-05-30 20:17:02 +0300

    30.05.2019🌿20:17

--- scripts/telegram.js
+++ scripts/telegram.js
@@ -83,8 +83,8 @@ let create_notifications_queue = async () => {
     notifications.map(async i => {
         let user = await new Parse.Query(`User`).equalTo(`objectId`, i.get(`user_id`)).first()
         if (user.get(`telegram`)) {
+            console.log((+moment(i.get(`delivery_timestamp`)).tz(`Europe/Moscow`) - +moment()) / 1000);
             setTimeout(() => {
-                console.log((+moment(i.get(`delivery_timestamp`)).tz(`Europe/Moscow`) - +moment()) / 1000);
                 telegram.sendMessage(user.get(`telegram`).id, i.get(`message`))
             }, +moment(i.get(`delivery_timestamp`)).tz(`Europe/Moscow`) - +moment())
         }
