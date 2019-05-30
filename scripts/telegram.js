d689f3a227d76b749f90e3100c7e653be47450c7
mityabeldii <mityabeldii@gmail.com>
2019-05-30 21:17:01 +0300

    30.05.2019🌿21:17

--- scripts/telegram.js
+++ scripts/telegram.js
@@ -33,7 +33,7 @@ let auth_command = () => {
         auth_mode = true
         ctx.reply(`Введите вашу почту (на домене @phystech.edu)`)
         bot.on(`text`, (mail_answer) => {
-            console.log(`> > >`);
+            console.log(`> > >`, auth_mode);
             if (auth_mode) {
                 let mail = mail_answer.update.message.text
                 mail.indexOf(`@`) && axios.get(`http://dcam.pro/api/auth/create_verificatoin_pass/${mail}/${ctx.update.message.from.id}/${ctx.update.message.from.username}`)
