2b95d122ad8dff60ab86bc5b6fbdb62728a80f38
mityabeldii <mityabeldii@gmail.com>
2019-06-03 12:49:21 +0300

    03.06.2019🌿12:49

--- deploy.sh
+++ deploy.sh
@@ -3,10 +3,7 @@ SSH_SERVER=104.248.28.15
 scp -r ./parse/* root@$SSH_SERVER:~/parse
 scp -r ./scripts/* root@$SSH_SERVER:~/scripts
 ssh root@$SSH_SERVER sh << END
-	# pm2 restart all
-	pm2 kill
-	pm2 start ./scripts/ecosystem_parse.json
-	pm2 start ./scripts/ecosystem.json
+	pm2 restart all
 END
 
 printf "\n\n\n> > > git commit (back) < < <\n\n\n"
