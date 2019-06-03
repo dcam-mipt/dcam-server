9675f8a43c409e8323e5e14fd2e3644b0c183dea
mityabeldii <mityabeldii@gmail.com>
2019-06-03 12:42:02 +0300

    03.06.2019🌿12:42

--- scripts/rest.js
+++ scripts/rest.js
@@ -79,7 +79,7 @@ let become = (request) => new Promise((resolve, reject) => {
         reject({
             error: `invalid sessoin token`,
             token: sessionToken,
-            request: Object.keys(request),
+            request: request._url,
             code: 209,
         }, sessionToken)
         return
