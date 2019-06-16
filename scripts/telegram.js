8e38c42d19976cc42149c1ab17d8a5c7c8b7b2f8
mityabeldii <mityabeldii@gmail.com>
2019-06-16 21:01:39 +0300

    16.06.2019🌿21:01

--- scripts/telegram.js
+++ scripts/telegram.js
@@ -36,7 +36,10 @@ let auth_command = () => {
         let users = await new Parse.Query(`User`).limit(1000000).find()
         let current_user = users.filter(i => i.get(`telegram`) !== undefined && i.get(`telegram`) !== null).map(i => { return { telegram_id: i.get(`telegram`).id, mail: i.get(`username`) } }).filter(i => +i.telegram_id === +ctx.update.message.from.id)[0]
         if (current_user) {
-            ctx.reply(`Этот бот уже привязан к профилю c почтой ${current_user.mail}`)
+            ctx.reply(`Этот бот уже привязан к профилю c почтой ${current_user.mail}`, Markup.inlineKeyboard([
+                Markup.callbackButton('Выйти', 'Выйти'),
+                Markup.callbackButton('Остаться', 'Остаться'),
+            ]).extra())
         } else {
             ctx.reply(`Введите вашу почту (на домене @phystech.edu)`)
             bot.on(`text`, (mail_answer) => {
@@ -125,121 +128,7 @@ bot.command('help', (ctx) => {
     /help - помощь в работе с сервисом`)
 })
 
-// > > >
-
 bot.use(Telegraf.log())
-
-bot.command('onetime', ({ reply }) =>
-    reply('One time keyboard', Markup
-        .keyboard(['/simple', '/inline', '/pyramid'])
-        .oneTime()
-        .resize()
-        .extra()
-    )
-)
-
-bot.command('custom', ({ reply }) => {
-    return reply('Custom buttons keyboard', Markup
-        .keyboard([
-            ['🔍 Search', '😎 Popular'], // Row1 with 2 buttons
-            ['☸ Setting', '📞 Feedback'], // Row2 with 2 buttons
-            ['📢 Ads', '⭐️ Rate us', '👥 Share'] // Row3 with 3 buttons
-        ])
-        .oneTime()
-        .resize()
-        .extra()
-    )
-})
-
-bot.hears('🔍 Search', ctx => ctx.reply('Yay!'))
-bot.hears('📢 Ads', ctx => ctx.reply('Free hugs. Call now!'))
-
-bot.command('special', (ctx) => {
-    return ctx.reply('Special buttons keyboard', Extra.markup((markup) => {
-        return markup.resize()
-            .keyboard([
-                markup.contactRequestButton('Send contact'),
-                markup.locationRequestButton('Send location')
-            ])
-    }))
-})
-
-bot.command('pyramid', (ctx) => {
-    return ctx.reply('Keyboard wrap', Extra.markup(
-        Markup.keyboard(['one', 'two', 'three', 'four', 'five', 'six'], {
-            wrap: (btn, index, currentRow) => currentRow.length >= (index + 1) / 2
-        })
-    ))
-})
-
-bot.command('simple', (ctx) => {
-    return ctx.replyWithHTML('<b>Coke</b> or <i>Pepsi?</i>', Extra.markup(
-        Markup.keyboard(['Coke', 'Pepsi'])
-    ))
-})
-
-bot.command('inline', (ctx) => {
-    return ctx.reply('<b>Coke</b> or <i>Pepsi?</i>', Extra.HTML().markup((m) =>
-        m.inlineKeyboard([
-            m.callbackButton('Coke', 'Coke'),
-            m.callbackButton('Pepsi', 'Pepsi')
-        ])))
-})
-
-bot.command('random', (ctx) => {
-    return ctx.reply('random example',
-        Markup.inlineKeyboard([
-            Markup.callbackButton('Coke', 'Coke'),
-            Markup.callbackButton('Dr Pepper', 'Dr Pepper', Math.random() > 0.5),
-            Markup.callbackButton('Pepsi', 'Pepsi')
-        ]).extra()
-    )
-})
-
-bot.command('caption', (ctx) => {
-    return ctx.replyWithPhoto({ url: 'https://picsum.photos/200/300/?random' },
-        Extra.load({ caption: 'Caption' })
-            .markdown()
-            .markup((m) =>
-                m.inlineKeyboard([
-                    m.callbackButton('Plain', 'plain'),
-                    m.callbackButton('Italic', 'italic')
-                ])
-            )
-    )
-})
-
-bot.hears(/\/wrap (\d+)/, (ctx) => {
-    return ctx.reply('Keyboard wrap', Extra.markup(
-        Markup.keyboard(['one', 'two', 'three', 'four', 'five', 'six'], {
-            columns: parseInt(ctx.match[1])
-        })
-    ))
-})
-
-bot.action('Dr Pepper', (ctx, next) => {
-    return ctx.reply('👍').then(() => next())
-})
-
-bot.action('plain', async (ctx) => {
-    ctx.editMessageCaption('Caption', Markup.inlineKeyboard([
-        Markup.callbackButton('Plain', 'plain'),
-        Markup.callbackButton('Italic', 'italic')
-    ]))
-})
-
-bot.action('italic', (ctx) => {
-    ctx.editMessageCaption('_Caption_', Extra.markdown().markup(Markup.inlineKeyboard([
-        Markup.callbackButton('Plain', 'plain'),
-        Markup.callbackButton('* Italic *', 'italic')
-    ])))
-})
-
-bot.action(/.+/, (ctx) => {
-    return ctx.answerCbQuery(`Oh, ${ctx.match[0]}! Great choice`)
-})
-
-// > > >
 
 bot.launch()
 /*eslint-enable no-unused-vars*/
