489d36287071cdf341597bffc255cae9d661b112
mityabeldii <mityabeldii@gmail.com>
2019-06-16 20:15:39 +0300

    16.06.2019🌿20:15

--- scripts/telegram.js
+++ scripts/telegram.js
@@ -116,8 +116,7 @@ subscribe(`Notifications`, `create`, async (notification) => {
 
 bot.start((ctx) => ctx.reply('Добро пожаловать!\n Отправьте /help, чтоб получить список команд'))
 bot.command('help', (ctx) => {
-    console.log(`> > > help > > >`);
-    ctx.replay(`Вот список команд:\n
+    ctx.reply(`Вот список команд:\n
     /auth - авторизация\n
     /help - помощь в работе с сервисом\n\n
     Отпарвьте боту любое сообщение с вопросом, мы постараемся ответить вам как можно быстрее.`)
