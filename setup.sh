f43b0a2de0ef99e7a799747e38c711cbaa84d87d
mityabeldii <mityabeldii@gmail.com>
2019-06-20 14:45:03 +0300

    20.06.2019🌿14:45

--- setup.sh
+++ setup.sh
@@ -1,61 +1,61 @@
 #!/bin/bash
 SSH_SERVER=104.248.28.15
-# ssh root@$SSH_SERVER sh << END
+ssh root@$SSH_SERVER sh << END
 
-#     printf '\n\n\n> > > ufw < < <\n\n\n'
+    printf '\n\n\n> > > ufw < < <\n\n\n'
 
-# 	sudo apt-get install ufw
-#     sudo ufw default deny incoming
-#     sudo ufw default allow outgoing
-#     sudo ufw allow ssh
-#     echo y | sudo ufw enable
-#     sudo ufw status verbose
-#     ufw allow 3000
-#     echo y | ufw enable
+	sudo apt-get install ufw
+    sudo ufw default deny incoming
+    sudo ufw default allow outgoing
+    sudo ufw allow ssh
+    echo y | sudo ufw enable
+    sudo ufw status verbose
+    ufw allow 3000
+    echo y | ufw enable
 
-#     printf '\n\n\n> > > nginx < < <\n\n\n'
+    printf '\n\n\n> > > nginx < < <\n\n\n'
 
-#     sudo apt update
-#     echo y | sudo apt install nginx
-#     sudo ufw app list
-#     sudo ufw allow 'Nginx HTTP'
-#     sudo ufw status
-#     sudo apt update
+    sudo apt update
+    echo y | sudo apt install nginx
+    sudo ufw app list
+    sudo ufw allow 'Nginx HTTP'
+    sudo ufw status
+    sudo apt update
 
-#     printf '\n\n\n> > > zip < < <\n\n\n'
-#     echo y | sudo apt-get install zip
+    printf '\n\n\n> > > zip < < <\n\n\n'
+    echo y | sudo apt-get install zip
 
-#     printf '\n\n\n> > > node < < <\n\n\n'
+    printf '\n\n\n> > > node < < <\n\n\n'
 
-#     echo Y | sudo apt install nodejs npm
-#     nodejs --version
-#     sudo apt install npm
+    echo Y | sudo apt install nodejs npm
+    nodejs --version
+    sudo apt install npm
 
-#     printf '\n\n\n> > > yarn < < <\n\n\n'
+    printf '\n\n\n> > > yarn < < <\n\n\n'
 
-#     curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
-#     echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
-#     sudo apt update
-#     echo Y | sudo apt install yarn
-#     sudo apt install --no-install-recommends yarn
-#     yarn --version
+    curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
+    echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
+    sudo apt update
+    echo Y | sudo apt install yarn
+    sudo apt install --no-install-recommends yarn
+    yarn --version
 
-#     printf '\n\n\n> > > pm2 < < <\n\n\n'
+    printf '\n\n\n> > > pm2 < < <\n\n\n'
 
-#     sudo npm install pm2@latest -g
+    sudo npm install pm2@latest -g
 
-#     printf '\n\n\n> > > mongo < < <\n\n\n'
+    printf '\n\n\n> > > mongo < < <\n\n\n'
 
-#     sudo apt update
-#     echo Y | sudo apt install mongodb
-#     sudo systemctl status mongodb
+    sudo apt update
+    echo Y | sudo apt install mongodb
+    sudo systemctl status mongodb
 
-#     rm -rf *
+    rm -rf *
 
-# END
+END
 
-# printf '\n\n\n> > > copy files to server < < <\n\n\n'
-# scp -r ./* root@$SSH_SERVER:~/
+printf '\n\n\n> > > copy files to server < < <\n\n\n'
+scp -r ./* root@$SSH_SERVER:~/
 
 ssh root@$SSH_SERVER sh << END
 
@@ -64,8 +64,9 @@ ssh root@$SSH_SERVER sh << END
     ./scripts/nginx.sh
 
     printf '\n\n\n> > > install packages && restart < < <\n\n\n'
-    cd parse && yarn install && cd ../
-    cd scripts && yarn install && cd ../
+    printf '\n> > > parse\n' && cd parse && yarn install && cd ../
+    printf '\n> > > scripts\n' && cd scripts && yarn install && cd ../
+    printf '\n> > > backup\n' && cd backup && yarn install && cd ../
     
     printf '\n\n\n> > > start pm2 ecosystem < < <\n\n\n'
     pm2 kill
