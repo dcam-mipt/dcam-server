d2c8a9fd53cea65c418d739c12d2dfecc9b9e73d
mityabeldii <mityabeldii@gmail.com>
2019-06-27 12:51:41 +0300

    27.06.2019🌿12:51

--- scripts/rest.js
+++ scripts/rest.js
@@ -641,7 +641,7 @@ let get_notifications = async (user_id) => {
     query.select(`user_id`, `message`, `delivery_timestamp`, `status`)
     let result = await query.find()
 
-    result.forEach(async (item) => {
+    await result.forEach(async (item) => {
         let username = (await new Parse.Query(`User`).equalTo(`objectId`, item.get(`user_id`)).first()).get(`username`)
         item.set(`username`, username)
     })
