04627dc56387158e4d1c418375ad008238e0d15c
mityabeldii <mityabeldii@gmail.com>
2019-06-27 23:50:47 +0300

    27.06.2019🌿23:50

--- scripts/rest.js
+++ scripts/rest.js
@@ -640,9 +640,8 @@ let get_notifications = async (user_id) => {
     }
     let notifications = await query.find()
     let users = await new Parse.Query(`User`).limit(1000000).find()
-    console.log(users.length);
     notifications.forEach(i => {
-        i.set(`user`, users.filter(u => u.get(`objectId`) === i.get(`user_id`)))
+        console.log(users.filter(u => u.get(`objectId`) === i.get(`user_id`)));
     })
     return notifications
 }
