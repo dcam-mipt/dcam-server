543a537cf3452991d00278968812cc10c781ff8d
mityabeldii <mityabeldii@gmail.com>
2019-07-04 15:23:37 +0300

    04.07.2019🌿15:23

--- backup/index.js
+++ backup/index.js
@@ -7,7 +7,7 @@ let interval_in_hours = 1 / (60 * 4)
 // setTimeout(() => {
 setInterval(() => {
     console.log(`> > >`);
-    exec("node upload.js", () => { console.log(`done`); })
+    exec("node ~/backup/upload.js", () => { console.log(`done`); })
 }, 60 * 60 * 1000 * interval_in_hours)
 // }, +moment().startOf(`minute`).add(1, `minute`).tz(`Europe/Moscow`) - +moment().tz(`Europe/Moscow`))
 /*eslint-enable no-unused-vars*/
