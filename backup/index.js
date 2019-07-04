e95c808ea6979cf32035aa0c15dbe2f69eba73b5
mityabeldii <mityabeldii@gmail.com>
2019-07-04 14:00:39 +0300

    04.07.2019🌿14:00

--- backup/index.js
+++ backup/index.js
@@ -2,5 +2,11 @@
 let moment = require(`moment-timezone`)
 let exec = require('child_process').exec;
 
+setTimeout(() => {
+    setInterval(() => {
+        console.log(`need backup`);
+    }, 1000)
+}, +moment().startOf(`minute`).add(1, `minute`).tz(`Europe/Moscow`) - +moment().tz(`Europe/Moscow`))
+
 exec("node upload.js", () => {})
 /*eslint-enable no-unused-vars*/
