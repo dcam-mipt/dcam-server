6f9bce6d33278efbf4cc1ec8053464257629e796
mityabeldii <mityabeldii@gmail.com>
2019-07-05 23:12:09 +0300

    05.07.2019🌿23:12

--- scripts/rest.js
+++ scripts/rest.js
@@ -97,7 +97,7 @@ let become = (request) => new Promise((resolve, reject) => {
 
 let isAdmin = (user) => new Promise((resolve, reject) => {
     new Parse.Query(`Roles`)
-        .equalTo(`user_id`, user.objectId)
+        .equalTo(`user_id`, user.id)
         .equalTo(`role`, `ADMIN`)
         .first()
         .then((d) => { resolve(true) })
