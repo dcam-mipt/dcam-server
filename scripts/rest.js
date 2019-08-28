b1bc048d44e13d59c05d7cf3084f2c0b61aab443
mityabeldii <mityabeldii@gmail.com>
2019-08-28 23:23:12 +0300

    28.08.2019🌿23:23

--- scripts/rest.js
+++ scripts/rest.js
@@ -717,8 +717,7 @@ server.post(`/targets/create/:name`, async (request, response, next) => {
         let user = await become(request)
         if (user) {
             console.log(`> > > 3`);
-            console.log(await is_admin(user));
-            if (await is_admin(user)) {
+            if (await isAdmin(user)) {
                 console.log(`> > > 4`);
                 const data = Array.from(Buffer.from(request.body, 'binary'))
                 const contentType = request.headers['content-type'];
