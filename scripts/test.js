6ae850d948d85d209f603a75c9fa1f8f3d82171d
mityabeldii <mityabeldii@gmail.com>
2019-11-06 00:01:01 +0300

    06.11.2019_🌿_00:01

--- scripts/test.js
+++ scripts/test.js
@@ -1,4 +1,4 @@
 var MailAPI = require('./MailAPI')
-MailAPI.sendEmail({ email: 'akhiyarov.fr@phystech.edu', subject: `привет`, html: `привет` })
+MailAPI.sendEmail({ email: 'kubrakov.ns@phystech.edu', subject: `привет`, html: `привет` })
     .then((d) => { console.log(d) })
     .catch((d) => { console.log(d) })
