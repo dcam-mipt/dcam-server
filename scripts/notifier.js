cd303f1c58c8c1fae02ec6af18dff596780cd516
mityabeldii <mityabeldii@gmail.com>
2019-12-04 11:35:39 +0300

    04.12.2019_🌿_11:35

--- scripts/notifier.js
+++ scripts/notifier.js
@@ -9,7 +9,20 @@ Parse.initialize(config.PARSE_APP_ID, config.PARSE_JS_KEY, config.PARSE_MASTER_K
 Parse.serverURL = config.PARSE_SERVER_URL
 Parse.User.enableUnsafeCurrentUser()
 
-let to_start_of_hour = +moment().startOf(`hour`).add(1, `hour`) - +moment()
-console.log(`> > >`);
-console.log(to_start_of_hour);
-console.log(`> > >`);
+let get_laundry = () => {
+    let laundry = new Parse.Query(`Laundry`)
+        .greaterThanOrEqualTo(`timestamp`, +moment().tz(`Europe/Moscow`).startOf(`isoWeek`))
+        .limit(1000000)
+        .find()
+}
+
+let to_start_of_hour = +moment().startOf(`minute`).add(1, `minute`) - +moment()
+setTimeout(() => {
+    setInterval(() => {
+        Mailer.sendEmail({ email: `beldiy.dp@phystech.edu`, subject: `dev`, html: `
+            <html>
+            <div>${moment().tz(`Europe/Moscow`).format(`HH:mm`)}</div>
+            </html>
+        ` })
+    }, 1000 * 60)
+}, to_start_of_hour)
